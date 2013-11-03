96001ee228273bb14b181669e5d7326266fa7999
MarwanNakhaleh <mnakhaleh@gmail.com>
2013-11-03 08:37:50 -0500

    cant follow yourself, dummypants

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,6 +4,6 @@ class Image < ActiveRecord::Base
 
   def self.from_users_followed_by(user)
   	followed_user_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
-  	where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", user_id: user.id)
+  	where("user_id IN (#{followed_user_ids})", user_id: user.id)
   end 
 end
