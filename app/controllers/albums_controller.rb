80c1c8c04817b210dfbd9e796abe44ee5c455c9d
Jonathan Arnett <jonarnett90@gmail.com>
2013-11-03 06:58:01 -0500

    Added basics to album controller

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,2 +1,34 @@
 class AlbumsController < ApplicationController
+  before_action :set_album, only: [:show, :edit, :update, :destroy]
+
+  def show
+  end
+
+  def new
+    @album = Album.new
+  end
+
+  def create
+    @album = current_user.albums.create(album_params)
+  end
+
+  def edit
+  end
+
+  def update
+    @album.update(album_params)
+  end
+
+  def destroy
+    @album.destroy
+  end
+
+  private
+    def set_album
+      @album = Album.find(params[:id])
+    end
+
+    def album_params
+      params.require(:album).permit(:title, :user_id)
+    end
 end
