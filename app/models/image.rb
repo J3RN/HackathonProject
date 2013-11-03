14f89b3632ffe416d9950dfc0bd381e0b5bcb59c
MarwanNakhaleh <mnakhaleh@gmail.com>
2013-11-02 23:22:17 -0400

    removed image crap

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,11 +2,4 @@ class Image < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :album
 	has_many :loves
-
-	def self.save
-		name = upload['image'].original_filename
-		directory = "public/images"
-		path = File.join(directory, name)
-		file.open(path, "wb") {|f| f.write(upload['image'].read) }
-	end
 end
