6414b2edcb0e2192272fe415a6f56d5164e02403
MarwanNakhaleh <mnakhaleh@gmail.com>
2013-11-03 08:47:55 -0500

    new album button, kind of a necessity

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 20131103112649) do
     t.integer  "user_id"
   end
 
+  create_table "artworks", force: true do |t|
+    t.string   "uploaded_file_file_name"
+    t.string   "uploaded_file_content_type"
+    t.integer  "uploaded_file_file_size"
+    t.datetime "uploaded_file_updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "images", force: true do |t|
     t.string   "title"
     t.integer  "loves"
