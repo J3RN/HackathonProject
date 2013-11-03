7797f4f086c0b451140aad84c92989d969c9f488
MarwanNakhaleh <mnakhaleh@gmail.com>
2013-11-03 00:05:02 -0400

    helppppp

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-<<<<<<< HEAD
   
   protected
  
@@ -12,10 +11,9 @@ class ApplicationController < ActionController::Base
     else
       super
     end
-=======
+  end
 
   def after_sign_in_path_for(resource)
     dashboard_path
->>>>>>> upstream/develop
   end
 end
