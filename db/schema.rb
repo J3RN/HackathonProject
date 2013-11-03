047a234c794905f1a49a69f8312d5d9858849e37
MarwanNakhaleh <mnakhaleh@gmail.com>
2013-11-03 00:46:05 -0400

    deleted schema.rb waste

--- db/schema.rb
+++ db/schema.rb
@@ -19,17 +19,6 @@ ActiveRecord::Schema.define(version: 20131103024837) do
     t.datetime "updated_at"
   end
 
-  create_table "images", force: true do |t|
-    t.string   "caption"
-    t.string   "title"
-    t.integer  "love"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.binary   "data"
-    t.string   "filename"
-    t.string   "mime_type"
-  end
-
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
